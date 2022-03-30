ba2f1896ebbfec676dc244be219f2bd5364fb86b
Thaix2phomai <93390718+Thaix2phomai@users.noreply.github.com>
2022-03-30 18:20:39 +0700

    btvenha

--- main.js
+++ main.js
@@ -1,57 +1,82 @@
-//bài 1
-function calculateFactorial(n) {
-  if (n > 0) {
-    return n * calculateFactorial(n - 1);
-  } else {
-    return 1;
-  }
+let products = [
+    {
+        name: "Iphone 13 Pro Max", // Tên sản phẩm
+        price: 3000000, // Giá mỗi sản phẩm
+        brand: "Apple", // Thương hiệu
+        count: 2, // Số lượng sản phẩm trong giỏ hàng
+    },
+    {
+        name: "Samsung Galaxy Z Fold3",
+        price: 41000000,
+        brand: "Samsung",
+        count: 1,
+    },
+    {
+        name: "IPhone 11",
+        price: 15500000,
+        brand: "Apple",
+        count: 1,
+    },
+    {
+        name: "OPPO Find X3 Pro",
+        price: 19500000,
+        brand: "OPPO",
+        count: 3,
+    },
+]
+
+// 6
+function addProduct (arr, newName, newPrice, newBrand, newCount){
+    let newProduct = {
+        name: newName,
+        price: newPrice,
+        brand: newBrand,
+        count: newCount
+    }
+    arr.push(newProduct)
+    return arr
+
 }
 
-console.log(calculateFactorial(5));
+console.log(addProduct(products, "ádas", 195000, "Cheese", 2))
 
-//bài 2
-function reverseString(str) {
-  var newString = "";
-  for (var i = str.length - 1; i >= 0; i--) {
-    newString = newString + str[i];
-  }
 
-  return newString;
-
+//7
+function deleteProduct (arr){
+    let brand = "Samsung"
+    return arr.filter(product => product.brand != brand )
 }
 
-console.log(reverseString("hello"));
+console.log(deleteProduct(products))
 
-//bài 3
 
-function translate(country) {
-  switch (country) {
-    case "VN":
-      console.log("Xin chào");
-      break;
-    case "EN":
-      console.log("Hello");
-      break;
-    case "JP":
-      console.log("Konichiwa");
-      break;
-    case "TH":
-      console.log("Sawadee");
-      break;
-    case "FR":
-      console.log("Bonjour");
-      break;
-    default:
-      break;
-  }
+//8
+
+function sortbyprice1 (arr) {
+    return arr.sort((a,b) => a.price - b.price)
 }
+console.log(sortbyprice1(products))
 
-console.log(translate("EN"))
+//9
 
-//bài 4
- function subString (str){
-    let stringSlice = str.substring(0,10) + "...";
-    return stringSlice;
- }
+function sortbyprice2 (arr) {
+    return arr.sort((a,b) => b.price - a.price)
+}
+console.log(sortbyprice2(products))
 
- console.log(subString("quwelkrwjhelkrhernw"))
+
+//10
+
+function getTwoRandonProduct(arr) {
+    let newArr = []
+    while (newArr.length < 2){
+        let randomNum = Math.floor(Math.random() * arr.length);
+        if(!newArr.includes(arr[randomNum])) {
+            newArr.push(arr[randomNum]);
+        }
+    }
+   
+    return newArr;
+}
+console.log(getTwoRandonProduct(products));
+
